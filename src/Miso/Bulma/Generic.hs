14beaca0b8f0f55961c429f767061f6ef611e6c2
Frank Staals <frank@fstaals.net>
2023-12-30 13:00:06 +0100

    initial package

---
+++ src/Miso/Bulma/Generic.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings          #-}
+module Miso.Bulma.Generic
+  ( useBulmaRemote
+  , bulmaLink
+  , iconLink
+  , icon
+  ) where
+
+import           Miso
+import           Miso.String (MisoString)
+
+--------------------------------------------------------------------------------
+
+-- | Use bulma from a CDN
+useBulmaRemote :: View action
+useBulmaRemote = div_ [] [bulmaLink,iconLink]
+
+-- | Insert a bulma link
+bulmaLink :: View action
+bulmaLink = link_ [ rel_ "stylesheet"
+                   , href_ "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"
+                   , textProp "integrity" "sha256-vK3UTo/8wHbaUn+dTQD0X6dzidqc5l7gczvH+Bnowwk="
+                   , textProp "crossorigin" "anonymous"
+                   ]
+
+-- | Produce a linked icon
+iconLink :: View action
+iconLink = Miso.script_ [ src_ "https://use.fontawesome.com/releases/v5.3.1/js/all.js"
+                        , defer_ "true"
+                        ] mempty
+
+--------------------------------------------------------------------------------
+
+-- | Produce an icon
+icon    :: MisoString -> View action
+icon cs = i_ [ class_ cs, textProp "aria-hidden" "true"] []
---
+++ src/Miso/Bulma/Panel.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings          #-}
+module Miso.Bulma.Panel
+  ( panel
+  , panelBlock
+  , panelIcon
+  ) where
+
+import           Miso
+import           Miso.String (MisoString)
+import           Miso.Bulma.Generic
+
+--------------------------------------------------------------------------------
+
+panel :: [View action] -> View action
+panel = div_ [class_ "panel is-full"]
+
+panelBlock :: [View action] -> View action
+panelBlock = div_ [class_ "panel-block"]
+
+panelIcon   :: MisoString -> View action
+panelIcon i = span_ [ class_ "panel-icon"]
+                    [ icon i]
