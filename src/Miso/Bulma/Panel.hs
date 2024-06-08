c8dfc2e90ea89949c64907292eb0db4ccda0f483
Frank Staals <frank@fstaals.net>
2024-06-08 20:32:53 +0200

    updates to the panel

--- src/Miso/Bulma/Panel.hs
+++ src/Miso/Bulma/Panel.hs
@@ -3,6 +3,8 @@ module Miso.Bulma.Panel
   ( panel
   , panelBlock
   , panelIcon
+
+  , panelTabs
   ) where
 
 import           Miso
@@ -11,12 +13,28 @@ import           Miso.Bulma.Generic
 
 --------------------------------------------------------------------------------
 
-panel :: [View action] -> View action
-panel = div_ [class_ "panel is-full"]
+-- | Renders a panel
+panel                 :: [Attribute action]
+                      -> [View action] -- ^ The heading
+                      -> [View action] -- ^ The content
+                      -> View action
+panel ats heading chs =
+    nav_ ([class_ "panel"] <> ats)
+         ([p_ [class_ "panel-heading"] heading
+          ]
+          <> chs
+         )
 
+-- | A div that is a panel block
 panelBlock :: [View action] -> View action
 panelBlock = div_ [class_ "panel-block"]
 
+-- | A panel icon
 panelIcon   :: MisoString -> View action
 panelIcon i = span_ [ class_ "panel-icon"]
                     [ icon i [] ]
+
+
+-- | Panel tabs
+panelTabs :: [View action] -> View action
+panelTabs = p_ [class_ "panel-tabs"]
